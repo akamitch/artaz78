2c4eaf53c3c485f373e66956033de45def943924
Mitch <mitch@aka-root.com>
2018-09-12 17:07:47 +0300

    добавил jacson однако парсер readValue не хочет работать, происходит IOExeption

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 package com.test;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 	// http://blog.harrix.org/article/7348 по этой доке подключал gson
 
+        System.out.println("используя gson");
         User vasiliy = new User("Vasiliy", 30);
         Gson gson = new Gson();
         System.out.println(gson.toJson(vasiliy));
@@ -16,6 +20,29 @@ public class Main {
         User petr = gson.fromJson(s, User.class);
 
         System.out.println("name: " + petr.name + " age :" + petr.age);
-        
+
+        System.out.println("\n---------------------");
+        System.out.println("используя jacson");
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String sJ = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(vasiliy);
+            System.out.println(sJ);
+            s = sJ;
+        }
+        catch (IOException e){
+            System.out.println("jacson не смог перевести обьект в json");
+        }
+        try {
+            User petrJ = mapper.readValue(s, User.class);
+            System.out.println("name: " + petrJ.name + " age :" + petrJ.age);
+        }
+        catch (IOException e) {
+            System.out.println("jacson не смог распарсить строку: ");
+            System.out.println(s);
+        }
+
+
+
     }
 }
